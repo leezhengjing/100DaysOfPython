e0088957be5e2bb43737b4300b1debe18ba6e5ef
Lee Zheng Jing <zhengjing.lee86@gmail.com>
2022-02-02 21:21:25 +0800

    day 22 project

--- Day_20and21_SnakeGame/snake.py
+++ Day_20and21_SnakeGame/snake.py
@@ -28,7 +28,6 @@ class Snake:
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
-
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
