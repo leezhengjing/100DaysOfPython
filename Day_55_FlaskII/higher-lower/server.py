35519d4c56f8330ed7f2fc8ccccfbe5944566ca8
Lee Zheng Jing <zhengjing.lee86@gmail.com>
2022-06-09 21:15:40 +0800

    day 55 project - Flask higher lower changes

--- Day_55_FlaskII/higher-lower/server.py
+++ Day_55_FlaskII/higher-lower/server.py
@@ -3,6 +3,8 @@ from random import randint
 
 app = Flask(__name__)
 
+random_number = randint(0, 9)
+
 def make_h1(function):
     def wrapper():
         output = f"<h1>{function()}</h1>"
@@ -12,8 +14,6 @@ def make_h1(function):
 @make_h1
 @app.route('/')
 def hello_world():
-    global random_number
-    random_number = randint(0, 9)
     return "<h1>Guess a number between 0 and 9</h1>" \
            "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>"
 
@@ -28,7 +28,7 @@ def number_check(number):
                "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'>"
     else:
         return "<h1 style='color:green'>You found me!</h1>" \
-               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'"
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'>"
 
 if __name__ == "__main__":
     app.run(debug=True)
