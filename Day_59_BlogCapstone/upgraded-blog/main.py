a26c728933ddeb52141c68e4bfb044629c8dec28
Lee Zheng Jing <zhengjing.lee86@gmail.com>
2022-06-17 20:41:10 +0800

    day 59 project - Blog Capstone

--- Day_59_BlogCapstone/upgraded-blog/main.py
+++ Day_59_BlogCapstone/upgraded-blog/main.py
@@ -23,5 +23,15 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/post/<int:post_id>")
+def get_post(post_id):
+    requested_post = None
+    for post in data:
+        if post["id"] == post_id:
+            requested_post = post
+    return render_template("post.html", post=requested_post)
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
